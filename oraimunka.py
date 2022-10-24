3507119c98b4daa7299e895790653b8b66e06607
Hevesi <116150966+hevesiistvan@users.noreply.github.com>
2022-10-24 09:54:08 +0200

    Update oraimunka.py
    ez a joooo

--- oraimunka.py
+++ oraimunka.py
@@ -1,10 +1,10 @@
 #1.Feladat: Kérjünk be 5 számot, és írjuk ki hogy páros vagy páratlan.
 
-for x in rang(1,6,1)
+for x in range(1,6,1)
 
-szam = int(inpt("Kérlek adj meg egy számot:"))
+    szam = int(input("Kérlek adj meg egy számot:"))
 
-if zsam % 2 === 0:
-       print("Páros)
-                  else:
-print(Páratlan")
+    if szam % 2 == 0:
+        print("Páros")
+    else:
+        print("Páratlan")
