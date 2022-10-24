bf781281d09af4a00049ea0041498514bbd607f4
Pák <116150960+PakBandi2006@users.noreply.github.com>
2022-10-24 09:42:18 +0200

    Add files via upload

---
+++ oraimunka.py
@@ -0,0 +1,10 @@
+#1.Feladat: Kérjünk be 5 számot, és írjuk ki hogy páros vagy páratlan.
+
+for x in rang(1,6,1)
+
+szam = int(inpt("Kérlek adj meg egy számot:"))
+
+if zsam % 2 === 0:
+       print("Páros)
+                  else:
+print(Páratlan")
